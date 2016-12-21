45f95043ec6a2a0a36ced83d492176a9276bcd5e
Caleb <caleb.kleveter@gmail.com>
2016-12-21 09:54:54 -0600

    Added basic elements to MainView

--- Sources/App/Models/Main.swift
+++ Sources/App/Models/Main.swift
@@ -10,5 +10,8 @@ import Foundation
 import UIWebKit
 
 final class MainView {
-    
+    let head = UIElement(element: Element.head)
+    let header = UIElement(element: Element.header)
+    let section = UIElement(element: Element.section)
+    let footer = UIElement(element: Element.footer)
 }
