17616776c769e828b8ee0982abf1e74c7835ea9c
Caleb <caleb.kleveter@gmail.com>
2016-12-21 09:34:12 -0600

    Created Main.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "UIWebKit-Example",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/calebkleveter/UIWebKit.git", majorVersion: 1, minor: 0)
     ],
     exclude: [
         "Config",
---
+++ Sources/App/Models/Main.swift
@@ -0,0 +1,9 @@
+//
+//  Main.swift
+//  UIWebKit-Example
+//
+//  Created by Caleb Kleveter on 12/21/16.
+//
+//
+
+import Foundation
