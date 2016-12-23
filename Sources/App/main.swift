28c21d0a8333508c2b738b188d65a050fc5f60d8
Caleb <caleb.kleveter@gmail.com>
2016-12-23 11:34:02 -0600

    Removed excess args in drop.view.make in drop.get

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,7 +5,7 @@ let drop = Droplet()
 drop.get { req in
     let mainView = MainView()
     if let main = mainView.render(with: drop) {
-        return try drop.view.make(main, [])
+        return try drop.view.make(main)
     } else {
         throw Abort.custom(status: .internalServerError, message: "Unable to Create Page.")
     }
