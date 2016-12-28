5675eb73849db012134e85c3e51cb18991d9829c
Caleb <caleb.kleveter@gmail.com>
2016-12-28 15:57:08 -0600

    Updated UIWebKit version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "UIWebKit-Example",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/calebkleveter/UIWebKit.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/calebkleveter/UIWebKit.git", majorVersion: 2, minor: 0)
     ],
     exclude: [
         "Config",
